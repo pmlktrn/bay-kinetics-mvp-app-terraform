2e71ceb5565de4a1243b76af0c81afcba78b8aa4
pmlktrn <66727828+pmlktrn@users.noreply.github.com>
2025-07-01 01:38:57 +0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,81 +1,112 @@
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
-  tags = { Name = "${var.project}-${var.environment}-vpc" }
+  tags = {
+    Name        = "${var.project}-${var.environment}-vpc"
+    Environment = var.environment
+    Project     = var.project
+    Owner       = var.owner
+  }
 }
+
+# Availability Zones data
 
 data "aws_availability_zones" "azs" {}
 
+# Public Subnets
 resource "aws_subnet" "public" {
   for_each                = toset(var.public_subnets)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = each.value
   availability_zone       = data.aws_availability_zones.azs.names[index(var.public_subnets, each.value)]
   map_public_ip_on_launch = true
-  tags = { Name = "public-${each.value}" }
+  tags = {
+    Name = "${var.project}-${var.environment}-public-${each.value}"
+  }
 }
 
+# Private Subnets
 resource "aws_subnet" "private" {
   for_each          = toset(var.private_subnets)
   vpc_id            = aws_vpc.main.id
   cidr_block        = each.value
   availability_zone = data.aws_availability_zones.azs.names[index(var.private_subnets, each.value)]
-  tags = { Name = "private-${each.value}" }
+  tags = {
+    Name = "${var.project}-${var.environment}-private-${each.value}"
+  }
 }
 
+# DB Subnets
 resource "aws_subnet" "db" {
   for_each          = toset(var.db_subnets)
   vpc_id            = aws_vpc.main.id
   cidr_block        = each.value
   availability_zone = data.aws_availability_zones.azs.names[index(var.db_subnets, each.value)]
-  tags = { Name = "db-${each.value}" }
+  tags = {
+    Name = "${var.project}-${var.environment}-db-${each.value}"
+  }
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.project}-${var.environment}-igw"
+  }
 }
 
 # Elastic IP for NAT Gateway
 resource "aws_eip" "nat" {}
 
-# NAT Gateway to allow private subnets outbound
+# NAT Gateway
 resource "aws_nat_gateway" "natgw" {
   allocation_id = aws_eip.nat.id
   subnet_id     = values(aws_subnet.public)[0].id
+  tags = {
+    Name = "${var.project}-${var.environment}-natgw"
+  }
 }
 
-
+# Public Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+  tags = { Name = "${var.project}-${var.environment}-public-rt" }
 }
+
 resource "aws_route_table_association" "public_assoc" {
   for_each       = aws_subnet.public
   subnet_id      = each.value.id
   route_table_id = aws_route_table.public.id
 }
 
+# Private Route Table
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.natgw.id
   }
+  tags = { Name = "${var.project}-${var.environment}-private-rt" }
 }
+
 resource "aws_route_table_association" "private_assoc" {
   for_each       = aws_subnet.private
   subnet_id      = each.value.id
   route_table_id = aws_route_table.private.id
 }
 
+# Network ACLs for Public Subnets
 resource "aws_network_acl" "public_acl" {
   vpc_id     = aws_vpc.main.id
   subnet_ids = values(aws_subnet.public)[*].id
+  tags = { Name = "${var.project}-${var.environment}-public-nacl" }
 }
-resource "aws_network_acl_rule" "public_in" {
+
+resource "aws_network_acl_rule" "public_inbound" {
   network_acl_id = aws_network_acl.public_acl.id
   rule_number    = 100
   egress         = false
@@ -83,7 +114,8 @@ resource "aws_network_acl_rule" "public_in" {
   rule_action    = "allow"
   cidr_block     = "0.0.0.0/0"
 }
-resource "aws_network_acl_rule" "public_out" {
+
+resource "aws_network_acl_rule" "public_outbound" {
   network_acl_id = aws_network_acl.public_acl.id
   rule_number    = 100
   egress         = true
@@ -92,11 +124,14 @@ resource "aws_network_acl_rule" "public_out" {
   cidr_block     = "0.0.0.0/0"
 }
 
+# Network ACLs for Private Subnets
 resource "aws_network_acl" "private_acl" {
   vpc_id     = aws_vpc.main.id
   subnet_ids = values(aws_subnet.private)[*].id
+  tags = { Name = "${var.project}-${var.environment}-private-nacl" }
 }
-resource "aws_network_acl_rule" "private_in" {
+
+resource "aws_network_acl_rule" "private_inbound" {
   network_acl_id = aws_network_acl.private_acl.id
   rule_number    = 100
   egress         = false
@@ -104,7 +139,8 @@ resource "aws_network_acl_rule" "private_in" {
   rule_action    = "allow"
   cidr_block     = "0.0.0.0/0"
 }
-resource "aws_network_acl_rule" "private_out" {
+
+resource "aws_network_acl_rule" "private_outbound" {
   network_acl_id = aws_network_acl.private_acl.id
   rule_number    = 100
   egress         = true
