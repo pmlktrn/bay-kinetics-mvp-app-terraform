a9315ffe9969178e9fa2f32b61d80d313c16557c
pmlktrn <66727828+pmlktrn@users.noreply.github.com>
2025-07-01 01:09:49 +0800

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "postgres" {
   instance_class          = var.db_instance_class
   allocated_storage       = var.db_allocated_storage
   port                    = var.db_port
-  name                    = var.db_name
+  db_name                 = var.db_name
   username                = var.db_username
   password                = var.db_password
   db_subnet_group_name    = aws_db_subnet_group.db_group.name
