433e1454b329e1df21deb58c2be11e0fb833eee6
pmlktrn <66727828+pmlktrn@users.noreply.github.com>
2025-07-01 01:02:45 +0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -35,24 +35,6 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_db_instance" "postgres" {
-  identifier              = "postgres-db"
-  engine                  = "postgres"
-  engine_version          = var.db_engine_version
-  instance_class          = var.db_instance_class
-  allocated_storage       = var.db_allocated_storage
-  port                    = var.db_port
-  db_name                 = var.db_name
-  username                = var.db_username
-  password                = var.db_password
-  db_subnet_group_name    = aws_db_subnet_group.db_group.name
-  vpc_security_group_ids  = [aws_security_group.app_sg.id]
-  multi_az                = var.db_multi_az
-  storage_encrypted       = true
-  backup_retention_period = var.db_backup_retention_period
-  skip_final_snapshot     = true
-  apply_immediately       = true
-}
 resource "aws_nat_gateway" "natgw" {
   allocation_id = aws_eip.nat.id
   subnet_id     = values(aws_subnet.public)[0].id
